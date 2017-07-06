c45d361e0e3b78f1080d41345716acd6903f0317
hackeru <hackerudroids@gmail.com>
2017-07-06 18:59:49 +0300

    change

--- app/src/main/java/israel/locationaware/MapsActivity.java
+++ app/src/main/java/israel/locationaware/MapsActivity.java
@@ -1,8 +1,13 @@
 package israel.locationaware;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
@@ -13,13 +18,19 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.auth.FirebaseAuth;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
 
     private void weHaveAUser() {
-        Toast.makeText(this, "Hi", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "hey you", Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -46,13 +57,13 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         getSupportFragmentManager().
                 beginTransaction().
                 replace(R.id.frame1, mapFragment).
+                replace(R.id.frame2, new LocationFragment()).
                 commit();
 
         //tell me when the map is loaded:
         mapFragment.getMapAsync(this);
 
     }
-
 
 
     @Override
@@ -71,9 +82,57 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     @Override
     public void onMapReady(GoogleMap map) {
-        LatLng latLng = new LatLng(31.0866, 34.905);
+        addMarker(map);
+        setMyLocation(map);
+    }
+
+    private void setMyLocation(final GoogleMap map) {
+        Dexter.
+                withActivity(this).
+                withPermission(Manifest.permission.ACCESS_FINE_LOCATION).
+                withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse response) {
+                        //noinspection MissingPermission
+                        map.setMyLocationEnabled(true);
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse response) {
+                        if (response.isPermanentlyDenied()) {
+                            Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                                    Uri.parse("package:" + getPackageName()));
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(MapsActivity.this, "too bad ... Your dog will be lost", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permission, final PermissionToken token) {
+                        new AlertDialog.Builder(MapsActivity.this).setTitle("We Need Your Location").
+                                setMessage("Your dog might get lost , and we'll help you find it").
+                                setPositiveButton("Allow", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        token.continuePermissionRequest();
+                                    }
+                                }).setNegativeButton("Not now...", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(MapsActivity.this, "Too bad... c ya", Toast.LENGTH_SHORT).show();
+                            }
+                        }).
+                                show();
+                    }
+                }).
+                check();
+
+    }
+
+    private void addMarker(GoogleMap map) {
+        LatLng latLng = new LatLng(31.727340, 34.745185);
         map.addMarker(new MarkerOptions().position(latLng));
         map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 12));
-
     }
 }
